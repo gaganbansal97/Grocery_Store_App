7486660354df69bfdf3078f539be7d2860b8f789
gaganbansal97 <gagan.bansal97@gmail.com>
2021-01-07 12:26:56 +0530

    Initial Commit

---
+++ uom.py
@@ -0,0 +1,24 @@
+from sqlconn import get_sql_connection
+import pandas
+
+
+def getuom(connection):
+
+    cursor = connection.cursor()
+
+    query = ('SELECT * FROM uom')
+
+    response = []
+    cursor.execute(query)
+
+    for (uom_id,uom_name) in cursor:
+        response.append({
+            'uom_id': uom_id,
+            'uom_name': uom_name
+        })
+
+    return response
+
+if __name__ == '__main__':
+    connection = get_sql_connection()
+    print(getuom(connection))
